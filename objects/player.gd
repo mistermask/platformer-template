f3de9bd094242f796f8eb66d4b56041b32f9398a
mistermask <biggieboijosh@gmail.com>
2019-10-19 18:28:49 -0500

    set up tilesets, better platformer control, bigger camera

--- objects/player.gd
+++ objects/player.gd
@@ -2,19 +2,32 @@ extends KinematicBody2D
 
 var velocity = Vector2()
 
-const JUMP_SPEED = 100
-const ACCLERATION = 10
+const JUMP_SPEED = -500
+const ACCLERATION = 30
 const SPEED_CAP = 200
+const GRAVITY = 10
+const DEACCLERATION = 60
+const UP = Vector2(0,-1)
+
+var time_since_last_jump = 0
 
 func _physics_process(delta):
-	velocity.y += 50
+	velocity.y += GRAVITY
 	if Input.is_action_pressed("ui_right"):
-		velocity.x = min(velocity.x + ACCLERATION, SPEED_CAP)
+		velocity.x = max(velocity.x + ACCLERATION, SPEED_CAP)
 	elif Input.is_action_pressed("ui_left"):
 		velocity.x = min(velocity.x - ACCLERATION, -SPEED_CAP)
 	else:
-		#velocity.x = 
-		
+		if velocity.x > 0:
+			velocity.x = max(velocity.x - DEACCLERATION, 0)
+		elif velocity.x < 0:
+			velocity.x = min(velocity.x + DEACCLERATION, 0)
 	
-	move_and_slide(velocity)
+	if is_on_floor() or is_on_wall():
+		if Input.is_action_just_pressed("ui_up"):
+			velocity.y = JUMP_SPEED
+			time_since_last_jump = .3
+	
+	velocity = move_and_slide(velocity,UP)
+	time_since_last_jump -= delta
 	pass
