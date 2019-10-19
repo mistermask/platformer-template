833b3a7e54f04ae90e09e92a8a59cd9257089763
mistermask <biggieboijosh@gmail.com>
2019-10-18 20:13:28 -0500

    fixed movment

--- objects/player.gd
+++ objects/player.gd
@@ -1,26 +1,29 @@
 extends KinematicBody2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-var speed = 2000
-const gravity = 150
+var velocity = Vector2()
+const GRAVITY = -10
+var speed = 200
+var jump_speed = -450
+const out_of_bounds = 700
 
 func _physics_process(delta):
-	var velocity = Vector2()
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= speed * delta
+	velocity.y -= GRAVITY
 	if Input.is_action_pressed("ui_right"):
-		velocity.x += speed * delta
-	velocity.y += gravity * delta
-	move_and_collide(velocity)
-	
+		velocity.x = speed
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -speed
+	else:
+		velocity.x = 0
+		
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			print("ready")
+			velocity.y = jump_speed
+		else:
+			velocity.y = 0
+
+
+func _process(delta):
+	if(position.y > out_of_bounds):
+		position = Vector2(510,200)
+	move_and_slide(velocity,Vector2(0,-1))
